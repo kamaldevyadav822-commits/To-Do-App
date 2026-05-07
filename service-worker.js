33fd5f96bc6aea97fb01dbace1e40142239d5885
kamaldevyadav822-commits <kamaldevyadav822@gmail.com>
2026-05-07 07:47:37 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+// =========================
+// FILE: service-worker.js
+// =========================
+
+const CACHE_NAME = "todo-pwa-v1";
+
+const urlsToCache = [
+
+  "./",
+  "./index.html",
+  "./manifest.json"
+
+];
+
+// INSTALL
+self.addEventListener("install",(event)=>{
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+    .then((cache)=>{
+
+      return cache.addAll(urlsToCache);
+
+    })
+
+  );
+
+});
+
+// FETCH
+self.addEventListener("fetch",(event)=>{
+
+  event.respondWith(
+
+    caches.match(event.request)
+    .then((response)=>{
+
+      return response || fetch(event.request);
+
+    })
+
+  );
+
+});
